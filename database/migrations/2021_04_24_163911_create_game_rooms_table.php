b512f3681ddd173e2db94001268382ed138067fd
Joao Beleno De Oliveira <joao.beleno@gmail.com>
2021-04-25 02:43:06 -0300

    initial commit

--- database/migrations/2021_04_24_163911_create_game_rooms_table.php
+++ database/migrations/2021_04_24_163911_create_game_rooms_table.php
@@ -15,16 +15,16 @@ class CreateGameRoomsTable extends Migration
     {
         Schema::create('game_rooms', function (Blueprint $table) {
             $table->id();
-            $table->integer("Square1")->default(0);
-            $table->integer("Square2")->default(0);
-            $table->integer("Square3")->default(0);
-            $table->integer("Square4")->default(0);
-            $table->integer("Square5")->default(0);
-            $table->integer("Square6")->default(0);
-            $table->integer("Square7")->default(0);
-            $table->integer("Square8")->default(0);
-            $table->integer("Square9")->default(0);
-            $table->integer("victoriousPlayer")->default(0);
+            $table->string("Square1", 1)->default('');
+            $table->string("Square2", 1)->default('');
+            $table->string("Square3", 1)->default('');
+            $table->string("Square4", 1)->default('');
+            $table->string("Square5", 1)->default('');
+            $table->string("Square6", 1)->default('');
+            $table->string("Square7", 1)->default('');
+            $table->string("Square8", 1)->default('');
+            $table->string("Square9", 1)->default('');
+            $table->string("gameState", 30)->default('');
             $table->unsignedBigInteger('gamerTurn');
             $table->unsignedBigInteger('gamer1Id');
             $table->unsignedBigInteger('gamer2Id')->default(0);
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -1,9 +1,10 @@
 
 <head>
-    <link rel="stylesheet" type="text/css" href="css/style.css"/>    
+    <link rel="stylesheet" type="text/css" href="css/style.css"/> 
+    <meta name="csrf-token" content="{{ csrf_token() }}" />   
 </head>
 <style>
-    #jogo{
+    .jogo{
         width:603px;
         height:600px;
         border:solid 3px
@@ -22,24 +23,51 @@
     }
 </style>
 <body>
-        <div id="jogo">
+<div>
+<p><a href="{{URL::to('game')}}">Nuevo Juego</a></p>
+<h1>{{$gameRoom->gameState}}</h1>
+</div>
+        <div class="jogo" id='game'>
             <div class="linha">
-                <div class="casa" id="casa1"></div>
-                <div class="casa" id="casa2"></div>
-                <div class="casa" id="casa3"></div>
+                <div class="casa" id="Square1">{{$gameRoom->Square1}}</div>
+                <div class="casa" id="Square2">{{$gameRoom->Square2}}</div>
+                <div class="casa" id="Square3">{{$gameRoom->Square3}}</div>
             </div>
             <div class="linha">
-                <div class="casa" id="casa4"></div>
-                <div class="casa" id="casa5"></div>
-                <div class="casa" id="casa6"></div>
+                <div class="casa" id="Square4">{{$gameRoom->Square4}}</div>
+                <div class="casa" id="Square5">{{$gameRoom->Square5}}</div>
+                <div class="casa" id="Square6">{{$gameRoom->Square6}}</div>
             </div>
             <div class="linha">
-                <div class="casa" id="casa7"></div>
-                <div class="casa" id="casa8"></div>
-                <div class="casa" id="casa9"></div>
+                <div class="casa" id="Square7">{{$gameRoom->Square7}}</div>
+                <div class="casa" id="Square8">{{$gameRoom->Square8}}</div>
+                <div class="casa" id="Square9">{{$gameRoom->Square9}}</div>
             </div>
         </div>
         <div id="resultado"></div>
-        <p>{{$gameRoom->gamer1Id}}</p>
-        <p>{{$gameRoom->id}}</p>
+        <p>Jogador 1: {{$gameRoom->gamer1Id}}, Jogador 2: {{$gameRoom->gamer2Id}}, sala: {{$gameRoom->id}}
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
+        <script>
+            $('.casa').on('click',function(){ 
+
+                var squareId = $(this).attr('id');
+                $.ajax({ 
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    },
+                    url: '{{url("jugar")}}', 
+                    type: 'PUT', 
+                    data: {square:squareId , idGameRoom:{{$gameRoom->id}}},
+                    success: function(data) { 
+                        window.location.href = "{{URL::to('game/'.$gameRoom->id)}}"
+                    } 
+                }); 
+            });
+
+            $( ".casa" )
+            .mouseleave(function() {
+                window.location.href = "{{URL::to('game/'.$gameRoom->id)}}"
+             });
+        </script>
 </body>
